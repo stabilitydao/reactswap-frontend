db3d4d6a5eb1955b4e2fb190abd15756ab679d5d
Alien Deployer <m@a17.name>
2022-02-18 03:53:08 +0300

    content fix

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -405,7 +405,7 @@ const Farms: React.FC = ({ children }) => {
           </Flex>
         )}
         <div ref={observerRef} />
-        <StyledImage src="/images/decorations/3dpan.png" alt="Pancake illustration" width={120} height={103} />
+        {/* <StyledImage src="/images/decorations/3dpan.png" alt="Pancake illustration" width={120} height={103} /> */}
       </Page>
     </FarmsContext.Provider>
   )
--- src/views/Home/components/FarmsPoolsRow/index.tsx
+++ src/views/Home/components/FarmsPoolsRow/index.tsx
@@ -56,7 +56,7 @@ const FarmsPoolsRow = () => {
     }
 
     if (pool.sousId === 0) {
-      return t('Manual CAKE')
+      return t('Manual REACT')
     }
 
     return t('Stake %stakingSymbol% - Earn %earningSymbol%', {
